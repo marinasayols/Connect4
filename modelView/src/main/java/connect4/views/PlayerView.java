d22bd74ad6cf7dc16ca381cb0e83ccb3d6ec6a0b
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-09-23 13:24:12 +0200

    Fix responsabilities getPutTokenError

--- modelView/src/main/java/connect4/views/PlayerView.java
+++ modelView/src/main/java/connect4/views/PlayerView.java
@@ -1,13 +1,15 @@
 package connect4.views;
 
+import connect4.models.Board;
 import connect4.models.Error;
-import connect4.models.*;
+import connect4.models.Message;
+import connect4.models.Turn;
 import utils.ClosedInterval;
 import utils.Console;
 
 public class PlayerView {
     private final Board board;
-    private Turn turn;
+    private final Turn turn;
 
     public PlayerView(Board board) {
         this.board = board;
@@ -25,19 +27,19 @@ public class PlayerView {
         do {
             column = Console.getInstance().readInt(Message.GET_COLUMN.toString()) - 1;
             error = this.getPutTokenError(column);
+            new ErrorView().writeln(error);
         } while (!error.isNull());
         this.board.putToken(column, this.turn.getActiveColor());
     }
 
     public Error getPutTokenError(int column) {
-        Error error = Error.NULL;
         if (!new ClosedInterval(0, Board.COLUMNS - 1).isIncluded(column)) {
-            error = Error.WIDTH_OVERFLOW;
-        } else if (this.board.isColumnFull(column)) {
-            error = Error.FULL_COLUMN;
+            return Error.WIDTH_OVERFLOW;
         }
-        new ErrorView().writeln(error);
-        return error;
+        if (this.board.isColumnFull(column)) {
+            return Error.FULL_COLUMN;
+        }
+        return Error.NULL;
     }
 
     public void writeWinner() {
